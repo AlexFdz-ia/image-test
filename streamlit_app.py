2f81fdbc6b1b2ffa5eaa013d20ce1b66c557960e
AlexFdz-ia <alejandrofernandezromero.af@gmail.com>
2025-03-12 09:13:05 +0000

    upload-files

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,34 @@
+import json
 import streamlit as st
-from openai import OpenAI
+from PyPDF2 import PdfReader
+import google.generativeai as genai
+from langchain_community.document_loaders import CSVLoader, PyPDFLoader
+
+chat_history = []
+
+def read_file(file):
+
+    # Leer el archivo PDF
+    pdf_reader = PdfReader(file)
+    full_text = ""
+
+    # Extraer texto de cada página
+    for page in pdf_reader.pages:
+        full_text += page.extract_text()
+        
+    return full_text
+
+# Funcion para generar la respuesta de Gemini
+def generate_answer(chat_history, prompt):
+
+    # Añadimos al historial el ultimo mensaje del ususario
+    chat_history.append(f"User: {prompt}" + "\nAssistant:")
+
+    # Generamos la respuesta y la añadimos al historial
+    response = model.generate_content(chat_history).text
+    chat_history.append(response)
+
+    return response
 
 # Show title and description.
 st.title("📄 Document question answering")
@@ -8,9 +37,13 @@ st.write(
 )
 
 # Definición del formulario
+# Let the user upload a file via `st.file_uploader`.
+uploaded_file = st.file_uploader("Sube un archivo PDF", type=["pdf"])
+
 genre = st.text_input("Estilo de la obra: ", value="")
 vibe = st.text_input("Tono de la novela: ", value="")
 target = st.text_input("Público objetivo: ", value="")
+
 col1, col2 = st.columns(2)
 with col1:
     duration = st.number_input("Duración: ", min_value=0, value=0)
@@ -22,10 +55,18 @@ with col2:
 # Botón de envío
 submit = st.button(label="Generar")
 
-def check(genre, vibe, target, duration, num_characters, num_scene, num_chapters):
-    return (genre and vibe and target and duration and num_characters and num_scene and num_chapters and submit)
+def check(genre, vibe, target, duration, num_characters, num_scene, num_chapters, uploaded_file):
+    return (genre and vibe and target and duration and num_characters and num_scene and num_chapters and submit and uploaded_file)
 
-if not check(genre, vibe, target, duration, num_characters, num_scene, num_chapters):
+if not check(genre, vibe, target, duration, num_characters, num_scene, num_chapters, uploaded_file):
+
     st.info("Por favor, rellena todos los campos", icon="ℹ️")
+
 else:
-    st.write("hola")
+    
+    full_text = read_file(uploaded_file)
+
+    # Establecemos la API de Google
+    genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
+    # Seleccionamos el modelo a usar
+    model = genai.GenerativeModel('gemini-1.5-flash')
